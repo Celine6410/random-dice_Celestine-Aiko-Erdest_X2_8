8830f7c9ee86cd3e4b818c796352dc8c9f96ed45
Celine6410 <celestineaikoerdest@gmail.com>
2026-02-12 07:30:25 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+function SetDicePattern () {
+    DiceValue = [
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . . . .
+        `)
+    ]
+}
+function RollDice () {
+    DiceValue[randint(0, 5)].showImage(0)
+}
+input.onGesture(Gesture.Shake, function () {
+    RollDice()
+})
+let DiceValue: Image[] = []
+SetDicePattern()
+RollDice()
